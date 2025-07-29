64d0354dbe74058e2903e0ecbd95745616ad7285
Aniket455 <aniketsgupta0302@gmail.com>
2025-07-29 19:24:22 +0530

    formatted code

--- src/testpackage/GitPractice.java
+++ src/testpackage/GitPractice.java
@@ -3,11 +3,9 @@ package testpackage;
 public class GitPractice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Test print");
-		int a =10;
-		int b= 10;
-		System.out.println(a+b);
+		int a = 10;
+		int b = 10;
+		System.out.println(a + b);
 	}
-
 }
